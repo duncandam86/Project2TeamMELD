b13b92da52f66ea8de1d7ffb231512f5c65eb6a0
Duncan Dam <duncandam@duncans-mbp.fsm.northwestern.edu>
2019-01-11 14:25:39 -0600

    update report.js

--- public/js/reports.js
+++ public/js/reports.js
@@ -2,36 +2,15 @@ $(document).ready(function () {
     //console.log("ok it's running");
     //submit form with validation and modal confirmation 
     $("#reportForm").validate({
-        submitHandler: function(form) {
+        submitHandler: function (form) {
             $("#submissionModal").modal('show');
             $('.modal-title').text('Your submission was successful')
             $('.modal-body').text('Thank you for your report.')
-            $("#reportForm").click(function(){
+            $("#reportForm").click(function () {
                 form.submit()
-            })    
-       
+            })
 
-        let newReport = {
-            username: $("#username").val().trim(),
-            address: $("#address").val().trim(),
-            city: $("#city").val().trim(),
-            state: $("#state").val().trim(),
-            zipcode: $("#zipcode").val().trim(),
-            violation_description: $("#violationDescription").val().trim(),
-            categories: $("#category").val().trim(),
-            rating: parseFloat($("#rating").val().trim())
-        }      
 
-        console.log(newReport);
-        
-        //Ajax call to post report back into the database
-        $.ajax("/api/reports", {
-            type: "POST",
-            data: newReport
-        }).then(function () {
-            console.log("New report is on database woohoo!");
-                   
-            
             let newReport = {
                 username: $("#username").val().trim(),
                 address: $("#address").val().trim(),
@@ -51,7 +30,7 @@ $(document).ready(function () {
                 data: newReport
             }).then(function () {
                 console.log("New report is on database woohoo!");
-                
+
                 $("#username").val("")
                 $("#address").val("")
                 $("#city").val("")
